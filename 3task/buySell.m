74b59c2ca56e0f9e5d56d9b8eb9b829256cd8244
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-04-07 17:49:21 +0300

    Added buy sell logic

--- 3task/buySell.m
+++ 3task/buySell.m
@@ -1,6 +1,31 @@
-function [hasCollapsed] =  buySell(data1, data2, startIndex, endIndex)
-    hasCollapsed.values(1) = 0;
+function [hasCollapsed] =  buySell(data1, data2, data, startIndex, endIndex, money)
+    hasCollapsed.values(startIndex:endIndex) = 0;
+    hasCollapsed.money = money;
+    hasCollapsed.shares = 0;
     for i=(startIndex+1):endIndex
-        hasCollapsed.values(i) = checkIfCollapsed(data1, data2, i);
+        if(checkIfCollapsed(data1, data2, i) == 1) 
+            if(hasCollapsed.money <= 0)
+                continue;
+            end
+            price = data.close(i);
+            sub = floor(money / price);
+            hasCollapsed.money = hasCollapsed.money - (sub * price);
+            hasCollapsed.shares = hasCollapsed.shares + sub;
+            hasCollapsed.values(i) = 1;
+        end
+        if(checkIfCollapsed(data1, data2, i) == -1) 
+            if(hasCollapsed.shares <= 0) 
+                continue;
+            end
+            price = data.close(i);
+            if(hasCollapsed.shares > 100)
+                sub = hasCollapsed.shares - 10;
+            else
+                sub = hasCollapsed.shares - 1;
+            end
+            hasCollapsed.money = hasCollapsed.money + (sub * price);
+            hasCollapsed.shares = hasCollapsed.shares - sub;
+            hasCollapsed.values(i) = -1;
+        end
     end
 end
--- 3task/main.m
+++ 3task/main.m
@@ -2,6 +2,8 @@ day = 'C:/Users/smlge/Desktop/FI/intcdaily.csv';
 dayData = readData(day, 'day');
 data = createStruct(dayData, 'day');
 
+money = 1000;
+
 interval = 9;
 startIndex = 1:(length(data.high)-interval);
 endIndex = interval:length(data.high);
@@ -9,5 +11,5 @@ endIndex = interval:length(data.high);
 wmaData = weightedMovingAverage(startIndex, endIndex, interval, data);
 lrData = linearRegression(startIndex, endIndex, interval, data);
 
-bs = buySell(wmaData.wma, lrData.y, 1, size(wmaData.wma, 2));
+bs = buySell(wmaData.wma, lrData.y, data, 1, size(wmaData.wma, 2), money);
 graphic(datenum(wmaData.dateTime), wmaData.wma, lrData.y, dayData, interval, bs.values);
